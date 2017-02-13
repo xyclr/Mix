7a138ab9659a2b133947027b77f36c77b2a677f6
xyclr <xyclr@163.com>
2017-02-13 17:25:18 +0800

    xxxx

--- www/index.php
+++
@@ -1,7 +0,0 @@
-<?php
-/* Get the config file. */
-$configFile = dirname(dirname(__FILE__)) . '/config/config.php';
-include $configFile;
-
-/* Locate to different entry according installed or not. */
-empty($config->installed) ?  header('location: sys/install.php') : header('location: sys/index.php');
